79e3093f8756b94039b27db894b373f1f0473e68
shohanurlion <rahmanliton387@gmail.com>
2024-05-31 08:20:36 +0600

    demo

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -23,6 +23,37 @@ import aboutimg from '../../assets/img/liton2.jpg'
                             <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit non vulputate. Aliquam metus tortor, auctor id gravida</h5>
                         </div>
                         </div>
+                        <div class="skills">
+            <div className="skill-name">
+              <p>Web Design</p>
+              <p>85%</p>
+            </div>
+            <div className="progress">
+              <div className="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style={{width:"85%"}}></div>
+            </div>
+            <div className="skill-name">
+              <p>Web Development</p>
+              <p>95%</p>
+            </div>
+            <div className="progress">
+              <div className="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style={{width:"85%"}}>
+              </div>
+            </div>
+            <div className="skill-name">
+              <p>Apps Design</p>
+              <p>90%</p>
+            </div>
+            <div className="progress">
+              <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style={{width:"85%"}}></div>
+            </div>
+            <div className="skill-name">
+              <p>Apps Development</p>
+              <p>85%</p>
+            </div>
+            <div className="progress">
+              <div className="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style={{width:"85%"}}></div>
+            </div>
+          </div>
                         <a href="#" className='btn'>Learn More</a>
                     </div>
                     
